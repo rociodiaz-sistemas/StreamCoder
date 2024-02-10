82505a4b634760b8e17b795bbeb1b3c48818cdf2
Esmeralda Flores <116323123+FloresEsme@users.noreply.github.com>
2024-02-09 18:02:28 -0600

    Storybook installed (#8)
    * Storybook installed.
    
    * Storybook linked with chakra.

---
+++ .storybook/preview.ts
@@ -0,0 +1,19 @@
+import type { Preview } from '@storybook/react'
+import theme from '../src/theme'
+
+const preview: Preview = {
+    parameters: {
+        actions: { argTypesRegex: '^on[A-Z].*' },
+        controls: {
+            matchers: {
+                color: /(background|color)$/i,
+                date: /Date$/i,
+            },
+        },
+        chakra: {
+            theme,
+        },
+    },
+}
+
+export default preview
---
+++ src/stories/Chast.stories.tsx
@@ -0,0 +1,12 @@
+import Chat from "../components/Chat";
+
+export default {
+  title: 'Components/Chat',
+  component: Chat,
+  tags: ['autodocs'],
+}
+
+export const Normal = {
+  args: {
+  }
+}
