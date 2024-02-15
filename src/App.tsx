ebee6f39602c0a88973b463b5be5bb1aef24deda
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-02-15 17:23:40 -0600

    Chakra provider moved.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import Chat from './components/Chat'
-import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
-    <ChakraProvider>
-      <main className="grid min-h-screen place-items-center">
-        <Chat />
-      </main>
-    </ChakraProvider>
+    <main className="grid min-h-screen place-items-center">
+      <Chat />
+    </main>
   )
 }
 
