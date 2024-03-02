b192c9d1ebe0cc0dda78ceb174c459da9dff784d
Rory <rociodiaz.sistemas@gmail.com>
2024-03-02 15:17:37 -0800

    RD: Refactor type definitions and implementation of useThemeMapping hook
    Adjusted the type definitions in the useThemeMapping hook to correctly represent the structure of message styles. Updated the getMessageBoxStyles function to return the nested object representing the styles for the specified message type. Resolved type errors by ensuring that the return type aligns with the expected MessageStyles interface. Committing these changes to enhance clarity and maintain consistency in the codebase.

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,3 +1,5 @@
+import { Box } from '@chakra-ui/react'
+import useThemeMapping from '../hooks/useThemeMapping'
 import { MessageModel } from '../utils/models'
 
 type MessageProps = {
@@ -8,6 +10,7 @@ const ChatMessage = ({
     message: { author, content },
     className,
 }: MessageProps) => {
+    const themeInfo = useThemeMapping() // Use the useThemeMapping hook
     const Badges = author.badges.map((bg, i) => (
         <img
             key={i}
@@ -22,8 +25,11 @@ const ChatMessage = ({
         </span>
     )
 
+    const messageBoxStyle = themeInfo.getMessageBoxStyles('common') || {}
+
     return (
-        <div
+        <Box
+            borderColor={messageBoxStyle.borderColor}
             className={`text-[15px] py-1 px-2 rounded hover:bg-gray-500/30 leading-6 ${className}`}
         >
             <div className="inline-flex items-baseline">
@@ -31,7 +37,7 @@ const ChatMessage = ({
                 {Username}
             </div>
             <span className="ml-3 break-words">{content}</span>
-        </div>
+        </Box>
     )
 }
 
--- src/themes/theme.ts
+++ src/themes/theme.ts
@@ -14,6 +14,24 @@ export default {
             },
         },
     },
+    messageStyles: {
+        subscriber: {
+            borderColor: 'primary',
+            background: 'secondary',
+        },
+        bit: {
+            borderColor: 'primary',
+        },
+        highlighted: {
+            borderColor: 'primary',
+        },
+        VIP: {
+            borderColor: 'primary',
+        },
+        common: {
+            borderColor: 'primary',
+        },
+    },
     gradients: {
         NightSky:
             'linear-gradient(180deg, #141311 0%, #2D2E38 45.5%, #333850 60%, #553A77 100%)',
