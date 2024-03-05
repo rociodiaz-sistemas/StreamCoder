e1c45d1089da4d8b10a26785a245c27b21c72289
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-03-04 19:40:54 -0600

    Chat colors deleted.

--- src/themes/baseTheme.ts
+++ src/themes/baseTheme.ts
@@ -10,43 +10,7 @@ const baseTheme = extendTheme({
                 width: 200,
             },
         },
-        ChatMessage: {
-            common: {
-                color: '#F5F5F5',
-                backgroundColor: 'rgba(237, 30, 129, 0.35)'
-            },
-            highlighted: {
-                color: '#F5F5F5',
-                backgroundColor: 'linear-gradient(180deg, rgba(237, 30, 129, 0.55) 0%, rgba(254, 246, 172, 0.69) 100%)',
-            },
-            subscriber: {
-                color: '#FFFFFF',
-                backgroundColor: 'linear-gradient(0deg, rgba(237, 30, 129, 0.13), rgba(237, 30, 129, 0.13)), url(planet.png)',
-            },
-            bit: {
-                color: '#F5F5F5',
-                backgroundColor: 'rgba(237, 30, 129, 0.13)',
-            },
-            vip: {
-                color: "purple",
-                backgroundColor: "lavender",
-            },
-            // Add more message types as needed
-        }
     },
-    colors: {
-        commonText: "black",
-        commonBackground: "lightgray",
-        subscriberText: "blue",
-        subscriberBackground: "lightblue",
-        bitText: "yellow",
-        bitBackground: "lightyellow",
-        highlightedText: "white",
-        highlightedBackground: "orange",
-        vipText: "purple",
-        vipBackground: "lavender",
-        // Add more color variables as needed
-      },
 })
 
 export default baseTheme
