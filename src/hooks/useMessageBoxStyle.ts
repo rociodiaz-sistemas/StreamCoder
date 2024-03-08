cd456a161bde7571594d46c344b4b8fe17d735cf
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-03-07 21:02:40 -0600

    Chat box styles hook created.

---
+++ src/hooks/useMessageBoxStyle.ts
@@ -0,0 +1,18 @@
+import { useTheme } from '@chakra-ui/react'
+import { Author } from '../utils/models'
+
+const useMessageBoxStyle = (author: Author) => {
+    const theme = useTheme()
+    const baseStyle = theme
+
+    const getMessageBoxStyle = () => {
+        return baseStyle.messageStyles[author.type]
+    }
+
+    return {
+        borderColor: getMessageBoxStyle().borderColor,
+        background: getMessageBoxStyle().background,
+    }
+}
+
+export default useMessageBoxStyle
