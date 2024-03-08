47042198b077bbc6278ed78fcf4c1d84fb3660bd
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-03-07 21:28:37 -0600

    Afternoon chat colors added.

--- src/themes/afternoonTheme.ts
+++ src/themes/afternoonTheme.ts
@@ -14,6 +14,29 @@ export default {
             },
         },
     },
+    messageStyles: {
+        // Define message box styles based on user type
+        subscriber: {
+            borderColor: 'lila',
+            background: 'rosaPalido',
+        },
+        bit: {
+            borderColor: 'lila',
+            background: 'rosaPalido',
+        },
+        highlighted: {
+            borderColor: 'nightWhite',
+            background: 'nightSkyAfternoon',
+        },
+        VIP: {
+            borderColor: 'lila',
+            background: 'rosaPalido',
+        },
+        common: {
+            borderColor: 'RosaBarbie',
+            background: 'rositaBb',
+        },
+    },
     gradients: {
         AfternoonSky:
             'linear-gradient(180deg, #64509F 2.5%, #CEA9DB 32%, #EBDDB5 100%)',
