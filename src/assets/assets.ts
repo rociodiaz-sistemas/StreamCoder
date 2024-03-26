d0f7602da54ee3f5d0e9d20090c68f9bc57fb8bd
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-03-26 11:30:35 -0600

    gifs added.

--- src/assets/assets.ts
+++ src/assets/assets.ts
@@ -1,6 +1,8 @@
 export const gifs = {
     general: {
         bitGif: 'url("https://media.tenor.com/yGH5RwRTBwEAAAAj/stars-glitter.gif")',
+        subscriberGif:
+            'url("https://i.pinimg.com/originals/4f/7b/a0/4f7ba0e852e67829d3632a54a34bf207.gif")',
     },
     morningTheme: {
         bitgif: '',
