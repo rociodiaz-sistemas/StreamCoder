3a93bc85dd7d84d04bd949c2e101b2a76ff2d89b
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-03-26 13:12:07 -0600

    Gradients deleted from baseTheme

--- src/themes/baseTheme.ts
+++ src/themes/baseTheme.ts
@@ -15,12 +15,8 @@ const baseTheme = extendTheme({
         rosaBarbie: '#ED1E81',
         nightWhite: '#F5F5F5',
         rositaBb: 'rgba(237, 30, 129, 0.35)',
-        cuteCosmico:
-            'linear-gradient(180deg, rgba(237, 30, 129, 0.55) 0%, rgba(254, 246, 172, 0.69) 100%)',
         lila: '#9747FF',
         rosaPalido: 'rgba(245, 214, 211, 0.43)',
-        nightSkyAfternoon:
-            'linear-gradient(180deg, rgba(20, 19, 17, 0.76) 0%, rgba(45, 46, 56, 0.76) 45.5%, rgba(51, 56, 80, 0.76) 60%, rgba(85, 58, 119, 0.76) 100%);',
     },
 })
 
