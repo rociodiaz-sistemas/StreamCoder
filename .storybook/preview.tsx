8eda8a1c7c113dbc8432a9439d141f49a88d4040
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-03-27 12:24:25 -0600

    Storybook preview themes edited.

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -11,56 +11,60 @@ import afternoonTheme from '../src/themes/afternoonTheme'
 type ThemeType = 'default' | 'morning' | 'day' | 'afternoon'
 
 const preview: Preview = {
-    parameters: {
-        chakra: {
-            theme,
-            morningTheme,
-            dayTheme,
-            afternoonTheme
-        },
+  parameters: {
+    chakra: {
+      theme,
+      morningTheme,
+      dayTheme,
+      afternoonTheme
     },
-    globalTypes: {
-        theme: {
-            description: 'Global theme for components',
-            defaultValue: 'default' as ThemeType, // Set the default theme option
-            toolbar: {
-                title: 'default', // Label for the toolbar item
-                icon: 'heart', // Icon for the toolbar item
-                items: ['default', 'morning', 'day', 'afternoon'], // Available theme options
-                dynamicTitle: true, // Update title based on selected value
-            },
-        },
+  },
+  globalTypes: {
+    theme: {
+      description: 'Global theme for components',
+      defaultValue: 'default' as ThemeType, // Set the default theme option
+      toolbar: {
+        icon: 'heart', // Icon for the toolbar item
+        items: [
+          { value: 'morning', right: '🌤️', title: 'Morning' },
+          { value: 'day', right: '☀️', title: 'Day' },
+          { value: 'afternoon', right: '🌅', title: 'Afternoon' },
+          { value: 'default', right: '🌃', title: 'Night' },
+        ], // Available theme options
+        dynamicTitle: true, // Update title based on selected value
+      },
     },
-    decorators: [
-        (Story, context) => {
-            // Get the selected theme from the context
-            const selectedTheme = context.globals.theme as ThemeType
+  },
+  decorators: [
+    (Story, context) => {
+      // Get the selected theme from the context
+      const selectedTheme = context.globals.theme as ThemeType
 
-            // Update the Chakra UI theme based on the selected theme
-            let updatedTheme: any;
+      // Update the Chakra UI theme based on the selected theme
+      let updatedTheme: any;
 
-            switch (selectedTheme) {
-                case 'morning':
-                    updatedTheme = extendTheme(morningTheme);
-                    break;
-                case 'day':
-                    updatedTheme = extendTheme(dayTheme);
-                    break;
-                case 'afternoon':
-                    updatedTheme = extendTheme(afternoonTheme);
-                    break;
-                default:
-                    updatedTheme = extendTheme(theme);
-                    break;
-            }
+      switch (selectedTheme) {
+        case 'morning':
+          updatedTheme = extendTheme(morningTheme);
+          break;
+        case 'day':
+          updatedTheme = extendTheme(dayTheme);
+          break;
+        case 'afternoon':
+          updatedTheme = extendTheme(afternoonTheme);
+          break;
+        default:
+          updatedTheme = extendTheme(theme);
+          break;
+      }
 
-            return (
-                <ChakraProvider theme={updatedTheme}>
-                    <Story {...context} />
-                </ChakraProvider>
-            )
-        },
-    ],
+      return (
+        <ChakraProvider theme={updatedTheme}>
+          <Story {...context} />
+        </ChakraProvider>
+      )
+    },
+  ],
 }
 
 export default preview
