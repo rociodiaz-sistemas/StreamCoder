3c18065766a68c1eb8d5674dee1351d89dc1239e
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-03-29 12:57:07 -0600

    Base Theme colors added.

--- src/themes/baseTheme.ts
+++ src/themes/baseTheme.ts
@@ -17,6 +17,14 @@ const baseTheme = extendTheme({
         rositaBb: 'rgba(237, 30, 129, 0.35)',
         lila: '#9747FF',
         rosaPalido: 'rgba(245, 214, 211, 0.43)',
+        amarilloMorning: '#FFC926',
+        amarilloMorningBit: '#FFB800',
+        grisPalidoMorning: 'rgba(245, 245, 245, 0.56)',
+        azulMorado: '#664EFF',
+        blueDayCommon: '#0E3BB9',
+        grisPalidoDay: 'rgba(245, 245, 245, 0.85)',
+        grisPalidoMorningBit: 'rgba(244, 235, 255, 0.46)',
+        grisMorningSub: 'rgba(244, 235, 255, 0.64)',
     },
 })
 
