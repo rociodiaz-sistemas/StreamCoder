7a8c8300dc2580323b166ebd1f12d718e492edac
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-04-09 19:48:15 -0600

    Hooks formated with prettier and eslint.

--- src/hooks/useThemeMapping.ts
+++ src/hooks/useThemeMapping.ts
@@ -1,50 +1,50 @@
-import { useTheme } from '@chakra-ui/react'
+import { useTheme } from '@chakra-ui/react';
 
 interface GradientMappings {
-    [key: string]: string
+  [key: string]: string;
 }
 
 interface MessageStyles {
-    borderColor?: string
-    background?: string
-    gifBackground?: string
-    messageBoxStyle: string
-    backgroundGradient?: string
+  borderColor?: string;
+  background?: string;
+  gifBackground?: string;
+  messageBoxStyle: string;
+  backgroundGradient?: string;
 }
 
 interface ThemeInfo {
-    themeName: string
-    getGradient: () => string
-    getMessageBoxStyles: (type: string) => MessageStyles | undefined
+  themeName: string;
+  getGradient: () => string;
+  getMessageBoxStyles: (type: string) => MessageStyles | undefined;
 }
 
 const useThemeMapping = (): ThemeInfo => {
-    const theme = useTheme()
+  const theme = useTheme();
 
-    const gradientMappings: GradientMappings = {
-        nightTheme: theme.gradients.NightSky,
-        morningTheme: theme.gradients.MorningSky,
-        dayTheme: theme.gradients.DaySky,
-        afternoonTheme: theme.gradients.AfternoonSky,
-        // Add mappings for other themes as needed
+  const gradientMappings: GradientMappings = {
+    nightTheme: theme.gradients.NightSky,
+    morningTheme: theme.gradients.MorningSky,
+    dayTheme: theme.gradients.DaySky,
+    afternoonTheme: theme.gradients.AfternoonSky,
+    // Add mappings for other themes as needed
+  };
+
+  const messageStyles: MessageStyles = theme.messageStyles || {};
+
+  const themeName = theme.themeName || 'nightTheme'; // Default to 'nightTheme' if theme name is not found
+
+  const getGradient = (): string => {
+    return gradientMappings[themeName] || ''; // Retrieve the gradient based on the current theme
+  };
+
+  const getMessageBoxStyles = (type: string): MessageStyles | undefined => {
+    const styles = messageStyles && theme.messageStyles[type];
+    if (typeof styles === 'object') {
+      return styles;
     }
+    return undefined;
+  };
+  return { themeName, getGradient, getMessageBoxStyles };
+};
 
-    const messageStyles: MessageStyles = theme.messageStyles || {}
-
-    const themeName = theme.themeName || 'nightTheme' // Default to 'nightTheme' if theme name is not found
-
-    const getGradient = (): string => {
-        return gradientMappings[themeName] || '' // Retrieve the gradient based on the current theme
-    }
-
-    const getMessageBoxStyles = (type: string): MessageStyles | undefined => {
-        const styles = messageStyles && theme.messageStyles[type]
-        if (typeof styles === 'object') {
-            return styles
-        }
-        return undefined
-    }
-    return { themeName, getGradient, getMessageBoxStyles }
-}
-
-export default useThemeMapping
+export default useThemeMapping;
