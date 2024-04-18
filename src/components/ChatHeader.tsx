91ceb00d2f1018f0a815a78beb81fba4db233917
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-04-17 21:15:11 -0600

    Header component created.

---
+++ src/components/ChatHeader.tsx
@@ -0,0 +1,53 @@
+import { Flex, Box, Text } from '@chakra-ui/react';
+import { IconButton } from '@chakra-ui/react';
+import { ArrowRightIcon, CloseIcon } from '@chakra-ui/icons';
+import baseTheme from '../themes/baseTheme';
+
+import useThemeMapping from '../hooks/useThemeMapping';
+
+export const ChatHeader = () => {
+  const themeInfo = useThemeMapping(); // Use the useThemeMapping hook
+  const { backgroundColor, color } = themeInfo.getHeaderStyle(); // Destructure backgroundColor and color
+  const { border, borderColor } = baseTheme.components.Header.baseStyle;
+
+  return (
+    <Flex
+      justify={'space-between'}
+      direction={'row'}
+      maxW="400px"
+      h={'fit-content'}
+      px={1}
+      textColor={color}
+      bg={backgroundColor}
+      border={border}
+      borderColor={borderColor}
+    >
+      <Flex
+        align={'center'}
+        direction={'row'}
+        w={'fit-content'}
+        h={'fit-content'}
+      >
+        <IconButton
+          color={color}
+          aria-label="Resize"
+          colorScheme=""
+          icon={<ArrowRightIcon />}
+        />
+        <Box pl={2}>
+          <Text as="b" fontSize="20px">
+            Chat
+          </Text>
+        </Box>
+      </Flex>
+      <Box>
+        <IconButton
+          color={color}
+          aria-label="Close window"
+          colorScheme=""
+          icon={<CloseIcon />}
+        />
+      </Box>
+    </Flex>
+  );
+};
---
+++ src/stories/ChatHeader.stories.tsx
@@ -0,0 +1,11 @@
+import { ChatHeader } from '../components/ChatHeader';
+
+export default {
+  title: 'Components/ChatHeader',
+  component: ChatHeader,
+  tags: ['autodocs'],
+};
+
+export const Rosa = {
+  args: {},
+};
