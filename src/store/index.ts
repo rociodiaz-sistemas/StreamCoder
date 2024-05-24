e280dc63d2adb8a571177c9e0ca20ca31ca5edaa
Rocio Diaz <rociodiaz.sistemas@gmail.com>
2024-05-23 22:24:14 -0300

    updated store to manage all reducers and show rootreducer

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,14 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 import createSagaMiddleware from 'redux-saga';
+import { combineReducers } from 'redux'; // Import combineReducers from redux
 import { messagesReducer } from './slices/messageSlice';
 import websocketSaga from './sagas/websocketSaga';
 
 const sagaMiddleware = createSagaMiddleware();
 
+// Combine reducers to create rootReducer
+const rootReducer = combineReducers({
+  messages: messagesReducer,
+  // Add other reducers here if needed
+});
+
+// Define RootState type based on the return type of rootReducer
+export type RootState = ReturnType<typeof rootReducer>;
+
 const store = configureStore({
-  reducer: {
-    messages: messagesReducer,
-  },
+  reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(sagaMiddleware),
 });
