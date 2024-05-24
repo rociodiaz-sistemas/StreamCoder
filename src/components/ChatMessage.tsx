7796ac9dc08e3cf8b3b88091be6d4f348287f85b
Rocio Diaz <rociodiaz.sistemas@gmail.com>
2024-05-24 14:37:57 -0300

    first animation using motion

--- src/components/ChatMessage.tsx
+++ src/components/ChatMessage.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex } from '@chakra-ui/react';
+import { motion } from "framer-motion";
 
 import useThemeMapping from '../hooks/useThemeMapping';
 import { MessageModel } from '../utils/models';
@@ -36,29 +37,35 @@ const MessageBox = ({
   fontSize: string; // Define the fontSize prop
 }) => {
   return (
-    <Box
-      w="90%"
-      p="1em"
-      fontSize={fontSize}
-      bgImage={backgroundGradient ? undefined : gifBackground}
-      bgSize="cover"
-      bgPosition="center"
-      bgRepeat="no-repeat"
-      bgGradient={backgroundGradient}
-      borderWidth="1px"
-      borderColor={borderColor}
-      borderRadius="12px"
-      shadow={boxShadow ? '0px 4px 6px rgba(0, 0, 0, 0.25)' : 'none'}
-      bgColor={background}
+    <motion.div
+      initial={{ scale: 0.5, opacity: 0, y: -20 }} // Initial scale, opacity, and y position of the message
+      animate={{ scale: 1, opacity: 1, y: 0 }} // Animation when the message appears
+      transition={{ duration: 0.3 }} // Duration of the animation
     >
-      <Flex align="center" direction="row">
-        <Flex justify="center" w="fit-content" h="fit-content" pr={1}>
-          {Badges}
+      <Box
+        w="90%"
+        p="1em"
+        fontSize={fontSize}
+        bgImage={backgroundGradient ? undefined : gifBackground}
+        bgSize="cover"
+        bgPosition="center"
+        bgRepeat="no-repeat"
+        bgGradient={backgroundGradient}
+        borderWidth="1px"
+        borderColor={borderColor}
+        borderRadius="12px"
+        shadow={boxShadow ? '0px 4px 6px rgba(0, 0, 0, 0.25)' : 'none'}
+        bgColor={background}
+      >
+        <Flex align="center" direction="row">
+          {/* <Flex justify="center" w="fit-content" h="fit-content" pr={1}>
+            {Badges}
+          </Flex> */}
+          {Username}
         </Flex>
-        {Username}
-      </Flex>
-      <span dangerouslySetInnerHTML={{ __html: content }}></span>
-    </Box>
+        <span dangerouslySetInnerHTML={{ __html: content }}></span>
+      </Box>
+    </motion.div>
   );
 };
 
