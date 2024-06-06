eb11e37d782ee4688392ec7832bf4efea7d1cb7b
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-06-05 19:11:50 -0600

    ChatMessage with format.

--- src/components/chat-widget/chat-message/ChatMessage.tsx
+++ src/components/chat-widget/chat-message/ChatMessage.tsx
@@ -1,15 +1,12 @@
 import { MessageModel } from '../../../utils/models';
-import MessageBox from "./MessageBox";
-import { MessageContent } from "./MessageContent";
+import MessageBox from './MessageBox';
+import { MessageContent } from './MessageContent';
 
-type MessageProps = {
+export type MessageProps = {
   message: MessageModel;
 } & React.ComponentPropsWithRef<'div'>;
 
-const ChatMessage = ({
-  message
-}: MessageProps) => {
-
+const ChatMessage = ({ message }: MessageProps) => {
   return (
     <MessageBox messageType={message.type}>
       <MessageContent message={message} />
