29db3cf44988f7a0378f516f509db7fbbe10ec80
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-06-05 19:09:16 -0600

    Chat story functional.

--- src/stories/Chat.stories.tsx
+++ src/stories/Chat.stories.tsx
@@ -1,11 +1,24 @@
+import React from 'react';
+import { Meta } from '@storybook/react';
+import { Provider } from 'react-redux';
+import { ChatProps } from '../components/chat-widget/Chat';
 import Chat from '../components/chat-widget/Chat';
+import mockStore from './store.mock';
 
-export default {
+const meta: Meta<ChatProps> = {
   title: 'Widgets/Chat',
   component: Chat,
   tags: ['autodocs'],
 };
 
-export const Normal = {
-  args: {},
+export default meta;
+
+type Story = (args: ChatProps) => JSX.Element;
+
+export const Normal: Story = () => {
+  return (
+    <Provider store={mockStore}>
+      <Chat />
+    </Provider>
+  );
 };
