f3566f2037e567272245ed8f2fc565aafd1df71a
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-06-05 20:17:32 -0600

    MessageContent with format.

--- src/components/chat-widget/chat-message/MessageContent.tsx
+++ src/components/chat-widget/chat-message/MessageContent.tsx
@@ -5,20 +5,20 @@ import { Flex } from '@chakra-ui/react';
 import Username from './usernames/UserName';
 
 export const MessageContent = ({ message }: { message: MessageModel }) => {
-    const Badges = message.author.badges.map((badge, i) => (
-        <img
-            key={badge.name + i}
-            src={`${badge.imageUrl}`}
-            alt={`${badge.name} ${badge.version} badge`}
-        />
-    ));
+  const Badges = message.author.badges.map((badge, i) => (
+    <img
+      key={badge.name + i}
+      src={`${badge.imageUrl}`}
+      alt={`${badge.name} ${badge.version} badge`}
+    />
+  ));
 
-    return (
-        <div>
-            <Flex align="center" direction="row">
-                <Username displayName={message.author.displayName} color={message.author.color} type={message.type} />
-            </Flex>
-            <span dangerouslySetInnerHTML={{ __html: message.content }}></span>
-        </div>
-    )
+  return (
+    <div>
+      <Flex align="center" direction="row">
+        <Username displayName={message.author.displayName} color={message.author.color} type={message.type} />
+      </Flex>
+      <span dangerouslySetInnerHTML={{ __html: message.content }}></span>
+    </div>
+  );
 };
