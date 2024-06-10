c9bb2ccb6eae9b7b5acc0b2389f9a067e8005c29
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-06-10 17:28:21 -0600

    Animation with fomat

--- src/components/chat-widget/Animation.tsx
+++ src/components/chat-widget/Animation.tsx
@@ -2,16 +2,16 @@ import React from 'react';
 import { motion, AnimationProps } from 'framer-motion';
 
 type AnimationComponentProps = {
-    animationProps: AnimationProps;
-    children: React.ReactNode;
+  animationProps: AnimationProps;
+  children: React.ReactNode;
 };
 
 const Animation: React.FC<AnimationComponentProps> = ({ animationProps, children }) => {
-    return (
-        <motion.div {...animationProps}>
-            {children}
-        </motion.div>
-    );
+  return (
+    <motion.div {...animationProps}>
+      {children}
+    </motion.div>
+  );
 };
 
 export default Animation;
