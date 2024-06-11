7b4c14e80608d57e94906086b45e3f3b5345924b
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-06-10 19:23:07 -0600

    Unused import deleted.

--- src/components/chat-widget/Chat.tsx
+++ src/components/chat-widget/Chat.tsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React from 'react';
 import { Box, Flex } from '@chakra-ui/react';
 import useChatLiveModeScrolling from '../../hooks/useChatLiveModeScrolling';
 import { MessageModel } from '../../utils/models';
