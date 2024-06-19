516df5a1a9092d1262e3eb17c8235ca342ce00e0
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-06-18 19:26:28 -0600

    Chat story wrapped in a parent.

--- src/stories/Chat.stories.tsx
+++ src/stories/Chat.stories.tsx
@@ -2,12 +2,13 @@ import { Meta, StoryObj } from '@storybook/react';
 import { ChatProps } from '../components/chat-widget/Chat';
 import Chat from '../components/chat-widget/Chat';
 import withTimeSlider from '../../.storybook/decorators/withTimeSlider';
+import withDivWrapper from '../../.storybook/decorators/withDivWrapper';
 
 const meta: Meta<ChatProps> = {
   title: 'Widgets/Chat',
   component: Chat,
   tags: ['autodocs'],
-  decorators: [withTimeSlider],
+  decorators: [withTimeSlider, withDivWrapper],
 };
 
 export default meta;
@@ -17,9 +18,9 @@ type Story = StoryObj<typeof Chat>;
 export const Normal: Story = {
   args: {
     height: 7,
-    defaultHeight: 800,
-    width: 800,
-    defaultWidth: 800,
+    defaultHeight: 7,
+    width: 7,
+    defaultWidth: 7,
     time: 19,
   },
 };
