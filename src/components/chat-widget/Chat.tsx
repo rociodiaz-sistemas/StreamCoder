a40c882ec98a8def169afba1ccc451412373a0eb
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-07-25 08:21:09 -0600

    chat height bug fixed.

--- src/components/chat-widget/Chat.tsx
+++ src/components/chat-widget/Chat.tsx
@@ -72,7 +72,7 @@ const ChatMessagesBox = React.forwardRef<
   ));
 
   return (
-    <Box ref={ref} overflow="auto" mt="4" px="10px" pb="10px" maxH="70vh">
+    <Box ref={ref} overflow="auto" mt="4" px="10px" pb="10px">
       {MessageList}
     </Box>
   );
