c0445c0f4aebaf8b9a9f8341f4ea375874ed08c4
Rocio Diaz <rociodiaz.sistemas@gmail.com>
2024-08-27 19:42:23 -0300

    animated

--- src/components/chat-widget/chat-animations/AnimatedBackground.tsx
+++ src/components/chat-widget/chat-animations/AnimatedBackground.tsx
@@ -102,13 +102,13 @@ const cloudsConfig: CloudConfig[] = [
     scale: 1.2,
   },
 
-  // {
-  //   image: 'twilight-4.png',
-  //   position: { x: 0, y: 15 },
-  //   scrollSpeed: 0.02,
-  //   alphaRange: [0.3, 0.8],
-  //   scale: 1.2,
-  // },
+  {
+    image: 'twilight-4.png',
+    position: { x: 0, y: 15 },
+    scrollSpeed: 0.02,
+    alphaRange: [0.3, 0.8],
+    scale: 1.2,
+  },
 ];
 // {
 //   image: 'twilight-2.png',
