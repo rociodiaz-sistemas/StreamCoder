73a2bd2d07b37e95544f3e3c3e284841282896f7
Rocío Díaz (Rory) <rociodiaz.sistemas@gmail.com>
2024-08-29 17:48:47 -0300

    Revert "Parallax clouds"

--- src/components/background/BackgroundOverlay.tsx
+++ src/components/background/BackgroundOverlay.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { Box } from '@chakra-ui/react';
 import useDynamicGradientColor from '../../hooks/useDynamicGradient';
 import AnimatedBackground from '../chat-widget/chat-animations/AnimatedBackground';
-import CloudParallax from './parallax-clouds/ParallaxClouds';
-import { useTimeManager } from '../../store/contexts/TimeManagerContext';
 
 const BackgroundOverlay: React.FC = () => {
-  const { gradientColor } = useTimeManager();
+  const backgroundGradient = useDynamicGradientColor();
   return (
-    <Box pos="relative" w="100vw" h="100vh" bgGradient={gradientColor}>
+    <Box
+      pos="relative"
+      overflow="hidden" // Adjust as needed
+      w="100vw"
+      h="100vh"
+      bgGradient={backgroundGradient}
+    >
       <AnimatedBackground />
     </Box>
   );
