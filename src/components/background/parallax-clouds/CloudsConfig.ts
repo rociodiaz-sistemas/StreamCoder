63e4a3ec5a2ac6be1d4812390a65191a5fc05d4b
Esmeralda Flores <ing.esmeralda.of@gmail.com>
2024-08-29 18:23:49 -0600

    Add different clouds types

--- src/components/background/parallax-clouds/CloudsConfig.ts
+++ src/components/background/parallax-clouds/CloudsConfig.ts
@@ -1,6 +1,37 @@
 import { TimeRangeKey } from '../../../store/types';
 import { CloudConfig } from './ParallaxScene';
 
+export const RISING_CLOUD_CONFIG: CloudConfig[] = [
+  {
+    image: 'rising-3.png',
+    position: { x: 0, y: 70 },
+    scrollSpeed: 0.02,
+    alphaRange: [0.3, 0.8],
+    scale: 1.2,
+  },
+  {
+    image: 'rising-4.png',
+    position: { x: 0, y: 70 },
+    scrollSpeed: 0.04,
+    alphaRange: [0.3, 0.8],
+    scale: 1.2,
+  },
+  {
+    image: 'rising-5.png',
+    position: { x: 0, y: 70 },
+    scrollSpeed: 0.1,
+    alphaRange: [0.3, 0.8],
+    scale: 1.2,
+  },
+  {
+    image: 'rising-6.png',
+    position: { x: 0, y: 70 },
+    scrollSpeed: 0.04,
+    alphaRange: [0.3, 0.8],
+    scale: 1.2,
+  },
+];
+
 export const MORNING_CLOUD_CONFIG: CloudConfig[] = [
   {
     image: 'morning-3.png',
@@ -79,6 +110,24 @@ export const ALMOSTDUSK_CLOUD_CONFIG: CloudConfig[] = [
   },
 ];
 
+export const DUSK_CLOUD_CONFIG: CloudConfig[] = [
+  {
+    image: 'dusk-3.png',
+    position: { x: 0, y: 70 },
+    scrollSpeed: 0.02,
+    alphaRange: [0.3, 0.8],
+    scale: 1.2,
+  },
+
+  {
+    image: 'dusk-4.png',
+    position: { x: 0, y: 70 },
+    scrollSpeed: 0.04,
+    alphaRange: [0.3, 0.8],
+    scale: 1.2,
+  },
+];
+
 export const TWILIGHT_CLOUD_CONFIG: CloudConfig[] = [
   {
     image: 'twilight-5.png',
@@ -129,16 +178,16 @@ export const NIGHT_CLOUD_CONFIG: CloudConfig[] = [
 ];
 
 export const CLOUD_CONFIGS: { [key in TimeRangeKey]: CloudConfig[] } = {
-  Dawn: TWILIGHT_CLOUD_CONFIG,
-  Rising: TWILIGHT_CLOUD_CONFIG,
-  Morning: MORNING_CLOUD_CONFIG,
+  Dawn: ALMOSTDUSK_CLOUD_CONFIG,
+  Rising: RISING_CLOUD_CONFIG,
+  Morning: DAY_CLOUD_CONFIG,
   Day: DAY_CLOUD_CONFIG,
-  SuperDay: SUPERDAY_CLOUD_CONFIG,
+  SuperDay: SUPERDAY_CLOUD_CONFIG, // Maybe changhe gradient color to a lighter one(?
   DayAlmostDusk: ALMOSTDUSK_CLOUD_CONFIG,
-  Dusk: TWILIGHT_CLOUD_CONFIG,
+  Dusk: DUSK_CLOUD_CONFIG,
   Twilight: TWILIGHT_CLOUD_CONFIG,
   Night: NIGHT_CLOUD_CONFIG,
-  DeepNight: TWILIGHT_CLOUD_CONFIG,
-  Midnight: TWILIGHT_CLOUD_CONFIG,
-  MidnightMidnight: TWILIGHT_CLOUD_CONFIG,
+  DeepNight: NIGHT_CLOUD_CONFIG,
+  Midnight: MORNING_CLOUD_CONFIG,
+  MidnightMidnight: MORNING_CLOUD_CONFIG,
 };
